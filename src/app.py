f1345a8ed7114ea4cc2ea721127b958317236a75
Badara <badara.jammeh@hotmail.com>
2023-05-26 20:37:15 +0200

    added

---
+++ src/app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+def squared(input: int) -> int:
+    return input * input
+    # Todo: add function to return the squre of the input int.
+
+
+if __name__ == "__main__":
+    squared(100)
+
+
